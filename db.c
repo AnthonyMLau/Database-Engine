b3784698c8e79d04041332fc4f4e3c78fa625715
Anthony Lau <anthony.lau2000@live.com>
2019-10-11 15:30:29 -0400

    upgrade our prepare_statement function to parse arguments

--- db.c
+++ db.c
@@ -53,6 +53,16 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     if (strncmp(input_buffer->buffer,  "insert", 6) == 0){
         statement->type = STATEMENT_INSERT;
+        int args_assigned = scanf(
+          input_buffer->buffer, 
+          "insert %d %s %s", 
+          &(statement->row_to_insert.id),
+          statement->row_to_insert.username,
+          statement->row_to_insert.email
+        );
+        if (args_assigned < 3){
+          return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
